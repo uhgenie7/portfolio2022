daf40d8b0d0ae770b8af2430b153fd185f72ba25
HyejinUm <uhj1993@naver.com>
2022-07-22 18:18:24 +0900

    Feat: #1 RecoilRoot

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app';
+import ThemeProvider from './context/themeProvider';
+import { RecoilRoot } from 'recoil';
 import GlobalStyle from '@src/styles/globalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <ThemeProvider>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
     </>
   );
 }
