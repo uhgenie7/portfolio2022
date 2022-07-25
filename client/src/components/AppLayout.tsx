59d914dc4e8b963ae20b00ab118071eb5d0e7b13
HyejinUm <uhj1993@naver.com>
2022-07-25 10:34:15 +0900

    Feat: #5 Feature AppLayout

---
+++ client/src/components/AppLayout.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react';
+
+const AppLayout: FC = ({ children }) => {
+  return (
+    <>
+      {/* nav */}
+      {/* body */}
+      {/* children */}
+      {/* footer */}
+    </>
+  );
+};
+
+export default AppLayout;
