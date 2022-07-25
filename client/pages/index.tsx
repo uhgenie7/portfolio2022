379e98574c8e09f702742798362fe0607e4d358e
HyejinUm <uhj1993@naver.com>
2022-07-25 11:04:01 +0900

    Feat: #8 Head || favicon

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,6 @@
-import Head from 'next/head';
-
 const Home = () => {
   return (
     <div>
-      <Head>
-        <title>setting-practice</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div>Home</div>
     </div>
   );
