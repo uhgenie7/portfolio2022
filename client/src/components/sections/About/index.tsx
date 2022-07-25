faf39e192654cb2f9e60f2bdf0e13785ad7da3e8
HyejinUm <uhj1993@naver.com>
2022-07-25 16:29:04 +0900

    Feat: #13 about section

---
+++ client/src/components/sections/About/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { useIsMobile } from '@src/hook/useIsMobile';
+
+const About = () => {
+  return <AboutSectionWrapper></AboutSectionWrapper>;
+};
+
+export default About;
+
+const AboutSectionWrapper = styled.section``;
