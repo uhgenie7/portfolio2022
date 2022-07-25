d61c1904aaa55e86fba931ab021d271be0cd834e
HyejinUm <uhj1993@naver.com>
2022-07-25 15:23:44 +0900

    Feat: #4 navigation

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,19 @@
-import '../src/styles/globalStyle';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../src/styles/globalStyle';
+
+const theme = {};
+
+export const decorators = [
+  (Story, context) => (
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story {...context} />
+      </ThemeProvider>
+    </RecoilRoot>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
