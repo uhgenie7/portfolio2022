0a1e760130a66fe1cf11a4201cc969ebf446e7f3
HyejinUm <uhj1993@naver.com>
2022-07-26 13:07:44 +0900

    feat: #14 feature carrer page

---
+++ client/src/components/sections/Carrer/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Section from '..';
+
+const Carrer = () => {
+  return (
+    <Section title="My Carrer" id="carrer">
+      <InnerWrapper></InnerWrapper>
+    </Section>
+  );
+};
+
+export default Carrer;
+
+const InnerWrapper = styled.div``;
