76be6750a9dce5eba525fc463f71cde5042cddf9
HyejinUm <uhj1993@naver.com>
2022-07-26 18:01:27 +0900

    Style: 컬러테마 추가

--- client/src/styles/variables.ts
+++ client/src/styles/variables.ts
@@ -3,7 +3,10 @@ import { css } from 'styled-components';
 const variables = css`
   :root {
     --main: #25b5aa;
+    --main2: #3e7d78;
     --sub: #00b9ef;
+    --snow: #d3d6dd;
+    --gray: #6e6f70;
     --success: #83e031;
     --info: #42c9ff;
     --warning: #ffc747;
